8f77905d14cbb2f7f2bbe412b480588f8f884901
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-08-16 13:29:35 +0900

    :hammer_and_wrench: Feat: 라우트 추가 (#7)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MyGlobalStyle } from "../src/style/GlobalStyle";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-//pages
+// pages
 import { SignUP } from "./pages/SignUpPage";
+import { Login } from "./pages/LoginPage";
+import { PostUpload } from "./pages/PostUpload";
 
 function App() {
   return (
     <BrowserRouter>
-      <MyGlobalStyle />
+      <div>
+        <Link to="/signup">
+          <button>회원가입 페이지</button>
+        </Link>
+        <Link to="/login">
+          <button>로그인 페이지</button>
+        </Link>
+        <Link to="/postupload">
+          <button>게시물 작성 페이지</button>
+        </Link>
+
         <Routes>
-          <Route path="/" element={<SignUP />} />
+          <Route path="/signup" element={<SignUP />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/postupload" element={<PostUpload />} />
         </Routes>
+      </div>
     </BrowserRouter>
   );
 }
