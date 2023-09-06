f76395eb40b2a924d1b30fa0d081e56a877e47cd
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-09-07 01:08:37 +0900

    :art: Design: 게시물 작성 - 페이지 단위로 컴포넌트 추가 (#23)

--- src/pages/post/PostInputInfoPage.jsx
+++ src/pages/post/PostInputInfoPage.jsx
@@ -1,151 +1,98 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-export function PostInputInfo({ selectedPicture, setStep }) {
+import { Input } from "../../components/common/post/Input";
+import { SelectedPicture } from "../../components/common/post/SelectedPicture";
 
-  const [postInfoFormData, setPostInfoFormData] = useState({
-    postText: "",
-    localLocation: "",
-    personTag: "",
-    selectFolder: "",
-    hashtag: "",
-  });
+import IconPhraseWrite from "../../assets/icon/icon-phrase-write.svg";
+import IconLocation from "../../assets/icon/icon-location.svg";
+import IconFolder from "../../assets/icon/icon-folder.svg";
+import IconHashTag from "../../assets/icon/icon-hash-tag.svg";
+import IconUserTag from "../../assets/icon/icon-user-tag.svg";
 
-  const handleData = (e) => {
-    const { id, value } = e.target;
-    setPostInfoFormData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
-  };
+export const PostInputInfoPage = ({ selectedPicture, setStep }) => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(selectedPicture, postInfoFormData);
-  };
+  // const [postInfoFormData, setPostInfoFormData] = useState({
+  //   postText: "",
+  //   localLocation: "",
+  //   personTag: "",
+  //   selectFolder: "",
+  //   hashtag: "",
+  // });
+
+  // const handleData = (e) => {
+  //   const { id, value } = e.target;
+  //   setPostInfoFormData((prevData) => ({
+  //     ...prevData,
+  //     [id]: value,
+  //   }));
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(selectedPicture, postInfoFormData);
+  // };
 
   return (
-    <>
-      <h1>PostUpload</h1>
-      <h2>정보입력</h2>
-      <PostInputInfoWrap>
-        <SelectedPictureWrap>
-          {selectedPicture.map((image, index) => (
-            <ul>
-              <li key={index}>
-                {<img src={`https://picsum.photos/200/19${image}`} alt={`Album ${index}`} />}
-              </li>
-            </ul>
-          ))}
-        </SelectedPictureWrap>
-        <FormWrap onSubmit={(e)=>{
-          handleSubmit(e);
-          setStep(4);
-          }}
-        >
-          <fieldset>
-            <label htmlFor="postText">게시글텍스트</label>
-            <input
-              type="text"
-              id="postText"
-              value={postInfoFormData.postText}
-              onChange={handleData}
-              placeholder="게시할 내용을 적어보세요"
-            ></input>
-            <label htmlFor="localLocation">위치검색</label>
-            <input
-              type="text"
-              id="localLocation"
-              value="임의값"
-              onClick={()=>setStep(2)}
-              style={{cursor: "pointer"}}
-              required
-              placeholder="게시물과 관련된 위치를 설정해보세요"
-            ></input>
-            <label htmlFor="personTag">사람태그</label>
-            <input
-              type="text"
-              id="personTag"
-              value={postInfoFormData.personTag}
-              onChange={handleData}
-              placeholder="함께한 사람이 있다면 태그해보세요"
-            ></input>
-            <label htmlFor="selectFolder">폴더지정</label>
-            <input
-              type="text"
-              id="selectFolder"
-              value="임의값"
-              style={{cursor: "pointer"}}
-              onClick={()=>setStep(3)}
-              placeholder="그룹화하고 싶은 폴더가있다면 선택해보세요"
-            ></input>
-            <label htmlFor="hashtag">해쉬태그</label>
-            <input
-              type="text"
-              id="hashtag"
-              value={postInfoFormData.hashtag}
-              onChange={handleData}
-              placeholder="해쉬태그를 입력해보세요"
-            ></input>
-          </fieldset>
-          <div>
-            <button type="submit">게시</button>
-          </div>
-        </FormWrap>
-      </PostInputInfoWrap>
-    </>
+    <WrapStyle>
+      <SelectedPicture />
+      <Input
+        type="text"
+        id="addPhraseWrite"
+        label={'문구 입력'}
+        placeholder="문구 입력..."
+        icon={IconPhraseWrite}
+        height={"100px"}
+        isTextarea={true}
+      >
+      </Input>
+      <Input
+        type="text"
+        id="addLocation"
+        label={'위치 추가'}
+        height={"50px"}
+        placeholder="위치 추가 (필수)"
+        icon={IconLocation}
+        onClick={() => setStep(1)}
+      >
+      </Input>
+      <Input
+        type="text"
+        id="addFolder"
+        label={'폴더 지정'}
+        placeholder="폴더 지정"
+        icon={IconFolder}
+        onClick={() => setStep(2)}
+      >
+      </Input>
+      <Input
+        type="text"
+        id="addHashTag"
+        label={'해쉬 태그'}
+        placeholder="해쉬 태그"
+        icon={IconHashTag}
+        onClick={() => setStep(3)}
+      >
+      </Input>
+      <Input
+        type="text"
+        id="addHumanTag"
+        label={'사람 태그'}
+        placeholder="사람 태그"
+        icon={IconUserTag}
+        onClick={() => setStep(4)}
+      >
+      </Input>
+    </WrapStyle>
   )
 }
 
-const PostInputInfoWrap = styled.div`
-  width: 600px;
-  margin: auto;
+const WrapStyle = styled.div`
   position: relative;
-`;
-
-const SelectedPictureWrap = styled.li`
-  list-style-type: none;
-  display: flex;
-  overflow-x: scroll;
-
-  & img {
-    width: 200px;
-    height: 200px;
-    vertical-align: top;
-    object-fit: cover;
-  }
-  & ul {
-    list-style-type: none;
-  }
-`;
-
-const FormWrap = styled.form`
-  width: 80%;
-  background-color: #756e6e;
+  width: 90%;
   margin: 0 auto;
-
-  & fieldset {
-    display: flex;
-    flex-direction: column;
-    border: none;
-    padding: 0;
-    &:nth-child(2) {
-      flex-direction: row;
-    }
-    & input {
-      margin-top: 10px;
-      padding: 5px;
-    }
-  }
-  & > div {
-    text-align: center;
-    & button {
-      display: inline-block;
-      background-color: white;
-      border: none;
-      margin-bottom: 10px;
-      cursor: pointer;
-    }
-  }
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 
--- src/pages/post/PostSelectPicturePage.jsx
+++ src/pages/post/PostSelectPicturePage.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-export function SelectPicture({ selectedPicture, setselectedPicture, setStep }) {
+import { CheckBox } from "../../components/common/post/CheckBox";
+
+export const PostSelectPicturePage = () => {
 
   const imageData = [
     { src: "https://picsum.photos/200/191" },
@@ -13,62 +15,47 @@ export function SelectPicture({ selectedPicture, setselectedPicture, setStep }) 
     { src: "https://picsum.photos/200/197" },
     { src: "https://picsum.photos/200/198" },
     { src: "https://picsum.photos/200/199" },
+    { src: "https://picsum.photos/200/191" },
+    { src: "https://picsum.photos/200/192" },
+    { src: "https://picsum.photos/200/193" },
+    { src: "https://picsum.photos/200/194" },
+    { src: "https://picsum.photos/200/195" },
+    { src: "https://picsum.photos/200/196" },
+    { src: "https://picsum.photos/200/197" },
+    { src: "https://picsum.photos/200/198" },
   ];
 
-  const handleCheckbox = (event) => {
-    const value = event.target.value;
-    if (selectedPicture.includes(value)) {
-      setselectedPicture(selectedPicture.filter(item => item !== value));
-    } else {
-      setselectedPicture([...selectedPicture, value]);
-    }
-  };
+  const [checkedBox, setCheckedBox] = useState([]);
 
   return (
     <>
-      <h1>PostUpload</h1>
-      <div>
-        <h2>선택한 이미지</h2>
-        <ul>
-          {selectedPicture.map((checkbox) => (
-            <li key={checkbox}>{checkbox}</li>
-          ))}
-        </ul>
-      </div>
+      <h1 className="a11y-hidden">PostUpload</h1>
       <AlbumWrap>
-        <ButtonWrap>
-        <button>촬영버튼</button>
-        <button 
-          onClick={()=>setStep(1)}
-        >다음버튼</button>
-        </ButtonWrap>
-      <AlbumListStyle>
-        {imageData.map((image, index) => (
-          <AlbumStyle key={index}>
-            <img src={image.src} alt={`Album ${index}`} />
-            <input
-              type="checkbox"
-              value={`${index + 1}`}
-              onChange={handleCheckbox}
-              checked={selectedPicture.includes(`${index + 1}`)}
-            />
-          </AlbumStyle>
-        ))}
-      </AlbumListStyle>
+        <AlbumListStyle>
+          {imageData.map((image, index) => (
+            <AlbumStyle key={index}>
+              <img src={image.src} alt={`Album ${index}`} />
+              <CheckBoxStyle>
+                <CheckBox index={index} checkedBox={checkedBox} setCheckedBox={setCheckedBox} />
+              </CheckBoxStyle>
+            </AlbumStyle>
+          ))}
+        </AlbumListStyle>
       </AlbumWrap>
     </>
   );
 }
 
 const AlbumWrap = styled.div`
-  width: 600px;
-  margin: auto;
   position: relative;
 `;
 
-const ButtonWrap = styled.div`
-  position: absolute;
-  right: 30px;
+const AlbumListStyle = styled.ul`
+  width: 90%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 5px;
 `;
 
 const AlbumStyle = styled.li`
@@ -80,20 +67,10 @@ const AlbumStyle = styled.li`
     vertical-align: top;
     object-fit: cover;
   }
-  & input { 
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    width: 30px;
-    height: 30px;
-  }
 `;
 
-const AlbumListStyle = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 8px;
-  padding: 30px;
+const CheckBoxStyle = styled.div`
+  position: absolute;
+  top: 7px;
+  right: 7px;
 `;
-
-
