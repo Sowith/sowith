304f99655c371a8222c9010ec9eb4b80ef371545
starcradle101 <dreamerdev12@gmail.com>
2023-09-15 15:50:16 +0900

    🎨Design: SearchBar의 뒤로가기 버튼 분리(#16) SearchBar 컴포넌트의 뒤로 가기 버튼을 재사용 가능성이 높다고 판단되어 BackButton이라는 이름의 컴포넌트로 분리 Related: #16

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,20 +1,16 @@
 import styled from 'styled-components';
 
-import arrowBack from '../../assets/icon/icon-arrow.svg';
+import { BackButton } from 'components/common/BackButton';
 import search from '../../assets/icon/icon-search.svg';
 
 export const SearchBar: React.FC = () => {
-  const handleBackButtonClick = (): void => {
-    console.log('BackButton has been clicked!');
-  };
-
   const handleSearchButtonClick = (): void => {
     console.log('SearchButton has been clicked!');
   };
 
   return (
     <SearchBarContainer>
-      <BackButton onClick={handleBackButtonClick} />
+      <BackButton />
       <SearchArea>
         <SearchInput type="text" placeholder="검색어를 입력하세요" />
         <SearchButton onClick={handleSearchButtonClick} />
@@ -28,17 +24,8 @@ const SearchBarContainer = styled.div`
   margin: 0 auto 20px auto;
   width: 90%;
   align-items: center;
-`;
 
-const BackButton = styled.button`
-  background-image: url(${arrowBack});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-  margin-right: 10px;
+  padding-top: 20px;
 `;
 
 const SearchArea = styled.div`
