8a8fe6564c2d7029a5b8b05de383711b7015eaea
starcradle101 <dreamerdev12@gmail.com>
2023-10-21 04:30:26 +0900

    🛠️Feat: 인기 폴더 및 그룹 페이지 firestore 연동(#22) 이제 인기 폴더 및 그룹 페이지에서 firestore를 연동해 데이터를 불러올 수 있습니다.

--- src/pages/search/SearchTrendingFolderPage.tsx
+++ src/pages/search/SearchTrendingFolderPage.tsx
@@ -1,97 +1,124 @@
-import { FC, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useFirestoreRead } from 'hooks/useFirestoreRead';
 import styled from 'styled-components';
+
 import { BackButton } from 'components/common/BackButton';
 import { SearchFolderItem } from 'components/search/SearchFolderItem';
 import { FolderDataItem } from 'components/search/SearchFolderItem';
 
-export const SearchTrendingFolder: FC = () => {
-  const [archiveFolderData, setArchiveFolderData] = useState<FolderDataItem[]>(
-    []
-  );
+export const SearchTrendingFolder: React.FC = () => {
+	const [archiveFolderData, setArchiveFolderData] = useState<FolderDataItem[]>(
+		[]
+	);
 
-  const handleLikeToggle = (folderId: string) => {
-    const updatedFolderData = [...archiveFolderData];
-    const folderIndex = updatedFolderData.findIndex(
-      (folder) => folder.folderId === folderId
-    );
+	const firestoreReader = useFirestoreRead('folders');
 
-    if (folderIndex !== -1) {
-      const folder = updatedFolderData[folderIndex];
-      folder.like = !folder.like;
-      if (folder.like) {
-        folder.likeCount += 1;
-      } else {
-        folder.likeCount -= 1;
-      }
-    }
-    setArchiveFolderData(updatedFolderData);
-  };
+	useEffect(() => {
+		const fetchTrendingFolders = async () => {
+			const response = await firestoreReader.ReadAllDocument();
+			const sortedByLikeCount = response.sort(
+				(a, b) => b.data.likeCount - a.data.likeCount
+			);
 
-  const handleBookmarkToggle = (folderId: string) => {
-    const updatedFolderData = [...archiveFolderData];
-    const folderIndex = updatedFolderData.findIndex(
-      (folder) => folder.folderId === folderId
-    );
+			const folderData: FolderDataItem[] = sortedByLikeCount.map((item) => ({
+				folderId: item.id,
+				folderImages: item.data.folderImages,
+				name: item.data.name,
+				likeCount: item.data.likeCount,
+				like: item.data.like,
+				bookmark: item.data.bookmark,
+				tags: item.data.tags,
+			}));
 
-    if (folderIndex !== -1) {
-      updatedFolderData[folderIndex].bookmark =
-        !updatedFolderData[folderIndex].bookmark;
-    }
-    setArchiveFolderData(updatedFolderData);
-  };
+			setArchiveFolderData(folderData);
+		};
 
-  return (
-    <Container>
-      <h1 className="a11y-hidden">인기 폴더 페이지</h1>
-      <TopNav>
-        <BackButton />
-        <p>인기 폴더</p>
-      </TopNav>
-      <TrendingFolder>
-        {archiveFolderData.map((folderItem) => (
-          <SearchFolderItem
-            key={folderItem.folderId}
-            data={folderItem}
-            onLikeToggle={handleLikeToggle}
-            onBookmarkToggle={handleBookmarkToggle}
-            folderImages={folderItem.folderImages[0]}
-          />
-        ))}
-      </TrendingFolder>
-    </Container>
-  );
+		fetchTrendingFolders();
+	}, []);
+
+	const handleLikeToggle = (folderId: string) => {
+		const updatedFolderData = [...archiveFolderData];
+		const folderIndex = updatedFolderData.findIndex(
+			(folder) => folder.folderId === folderId
+		);
+
+		if (folderIndex !== -1) {
+			const folder = updatedFolderData[folderIndex];
+			folder.like = !folder.like;
+			if (folder.like) {
+				folder.likeCount += 1;
+			} else {
+				folder.likeCount -= 1;
+			}
+		}
+		setArchiveFolderData(updatedFolderData);
+	};
+
+	const handleBookmarkToggle = (folderId: string) => {
+		const updatedFolderData = [...archiveFolderData];
+		const folderIndex = updatedFolderData.findIndex(
+			(folder) => folder.folderId === folderId
+		);
+
+		if (folderIndex !== -1) {
+			updatedFolderData[folderIndex].bookmark =
+				!updatedFolderData[folderIndex].bookmark;
+		}
+		setArchiveFolderData(updatedFolderData);
+	};
+
+	return (
+		<Container>
+			<h1 className='a11y-hidden'>인기 폴더 페이지</h1>
+			<TopNav>
+				<BackButton />
+				<p>인기 폴더</p>
+			</TopNav>
+			<TrendingFolder>
+				{archiveFolderData.map((folderItem) => (
+					<SearchFolderItem
+						key={folderItem.folderId}
+						data={folderItem}
+						onLikeToggle={handleLikeToggle}
+						onBookmarkToggle={handleBookmarkToggle}
+						folderImages={folderItem.folderImages[0]}
+					/>
+				))}
+			</TrendingFolder>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  width: 88%;
-  background-color: #ffffff;
-  margin: 0 auto;
-  padding-bottom: 10px;
+	width: 88%;
+	background-color: #ffffff;
+	margin: 0 auto;
+	padding-bottom: 10px;
 
-  section {
-    padding-top: 18px;
-  }
+	section {
+		padding-top: 18px;
+	}
 `;
 
 const TopNav = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  margin-top: 40px;
-  p {
-    font-size: 1.5rem;
-    font-family: var(--font--SemiBold);
-  }
+	display: flex;
+	align-items: center;
+	gap: 5px;
+	margin-top: 40px;
+	p {
+		font-size: 1.5rem;
+		font-family: var(--font--SemiBold);
+	}
 `;
 
 const TrendingFolder = styled.section`
-  width: 100%;
-  padding: 0;
-  gap: 5px;
-  box-sizing: border-box;
-  margin: 0 auto;
-  margin-top: 30px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+	width: 100%;
+	padding: 0;
+	gap: 5px;
+	box-sizing: border-box;
+	margin: 0 auto;
+	margin-top: 30px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
 `;
--- src/pages/search/SearchTrendingGroupPage.tsx
+++ src/pages/search/SearchTrendingGroupPage.tsx
@@ -1,53 +1,80 @@
-import { FC, useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useFirestoreRead } from 'hooks/useFirestoreRead';
 import styled from 'styled-components';
-import { BackButton } from 'components/common/BackButton';
-import { GroupUI } from 'components/common/GroupUI';
 
-export const SearchTrendingGroup: FC = () => {
-  return (
-    <Container>
-      <h1 className="a11y-hidden">인기 그룹 페이지</h1>
-      <TopNav>
-        <BackButton />
-        <p>인기 그룹</p>
-      </TopNav>
-      <TrendingGroup>
-        <GroupUI />
-        <GroupUI />
-        <GroupUI />
-        <GroupUI />
-        <GroupUI />
-      </TrendingGroup>
-    </Container>
-  );
+import { BackButton } from 'components/common/BackButton';
+import { GroupUI, GroupUIProps } from 'components/common/GroupUI';
+
+export const SearchTrendingGroup: React.FC = () => {
+	const [archiveGroupData, setArchiveGroupData] = useState<GroupUIProps[]>([]);
+
+	const firestoreReader = useFirestoreRead('groups');
+	useEffect(() => {
+		const fetchTrendingGroups = async () => {
+			const response = await firestoreReader.ReadAllDocument();
+
+			const groupdata: GroupUIProps[] = response.map((item) => ({
+				groupId: item.id,
+				groupTitle: item.data.name,
+				groupTags: item.data.tags,
+				groupMember: item.data.groupUsers.length,
+				groupImageURL: item.data.groupImageURL,
+			}));
+
+			setArchiveGroupData(groupdata);
+		};
+
+		fetchTrendingGroups();
+	}, []);
+
+	return (
+		<Container>
+			<h1 className='a11y-hidden'>인기 그룹 페이지</h1>
+			<TopNav>
+				<BackButton />
+				<p>인기 그룹</p>
+			</TopNav>
+			<TrendingGroup>
+				{archiveGroupData.map((groupItem) => (
+					<GroupUI
+						groupId={groupItem.groupId}
+						groupTitle={groupItem.groupTitle}
+						groupTags={groupItem.groupTags}
+						groupMember={groupItem.groupMember}
+						groupImageURL={groupItem.groupImageURL}
+					/>
+				))}
+			</TrendingGroup>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  width: 100%;
-  background-color: #ffffff;
-  margin: 0 auto;
-  padding-bottom: 10px;
+	width: 100%;
+	background-color: #ffffff;
+	margin: 0 auto;
+	padding-bottom: 10px;
 
-  section {
-    padding-top: 18px;
-  }
+	section {
+		padding-top: 18px;
+	}
 `;
 
 const TopNav = styled.div`
-  display: flex;
-  width: 88%;
-  align-items: center;
-  gap: 5px;
-  margin: 0 auto;
-  margin-top: 40px;
-  p {
-    font-size: 1.5rem;
-    font-family: var(--font--SemiBold);
-  }
+	display: flex;
+	width: 88%;
+	align-items: center;
+	gap: 5px;
+	margin: 0 auto;
+	margin-top: 40px;
+	p {
+		font-size: 1.5rem;
+		font-family: var(--font--SemiBold);
+	}
 `;
 
 const TrendingGroup = styled.section`
-  width: 100%;
-  margin: 0;
-  margin-top: 30px;
+	width: 100%;
+	margin: 0;
+	margin-top: 30px;
 `;
