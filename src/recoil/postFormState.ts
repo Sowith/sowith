67b40e6359809415baa54eb0beee739d0ee9691a
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-10-25 22:50:14 +0900

    :hammer_and_wrench: Feat: recoil Atom 추가 - recoil로 게시물 정보들을 저장 (임시저장 해놨다가 다시 재사용하기위해) (#53)

---
+++ src/recoil/postFormState.ts
@@ -0,0 +1,25 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist'
+
+const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : undefined
+
+const { persistAtom } = recoilPersist(
+  {
+    key: 'postForm',
+    storage: sessionStorage,
+  }
+);
+
+const postFormState = atom({
+  key: 'data',
+  default: {
+    phrase: "",
+    location: "",
+    folder: [] as string[],
+    hashtag: [] as string[],
+    usertag: [] as string[],
+  },
+  effects_UNSTABLE: [persistAtom],
+});
+
+export default postFormState;
