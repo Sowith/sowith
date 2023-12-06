43fc1d537e39f8c6aadc2fc4d10866174f184e6b
starcradle101 <dreamerdev12@gmail.com>
2023-12-06 16:53:36 +0900

    🛠️Feat: 검색 메인 메뉴에서 태그 클릭 시 검색 상세 페이지 이동 기능 구현(#22)

--- src/components/search/SearchTrendingTagItem.tsx
+++ src/components/search/SearchTrendingTagItem.tsx
@@ -1,43 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+
 import styled from 'styled-components';
 
 export interface TrendingTagData {
-  trendingTag: string;
-  tagNumber: number;
+	trendingTag: string;
+	tagNumber: number;
 }
 
 export const SearchTrendingTagItem: React.FC<TrendingTagData> = ({
-  trendingTag,
+	trendingTag,
 }) => {
-  return (
-    <Container>
-      <span># {trendingTag}</span>
-    </Container>
-  );
+	const navigate = useNavigate();
+	const handleTagClick = (): void => {
+		navigate('/searchbycategory', { state: trendingTag });
+	};
+
+	return (
+		<Container>
+			<span onClick={handleTagClick}># {trendingTag}</span>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  padding: 10px;
-  font-size: 0.8rem;
-  box-sizing: border-box;
-  border-radius: 20px;
-  cursor: pointer;
-  background-color: var(--main-color);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	white-space: nowrap;
+	padding: 10px;
+	font-size: 0.8rem;
+	box-sizing: border-box;
+	border-radius: 20px;
+	cursor: pointer;
+	background-color: var(--main-color);
 
-  span {
-    display: inline-block;
-    color: #ffffff;
-    font-family: var(--font--SemiBold);
-  }
+	span {
+		display: inline-block;
+		color: #ffffff;
+		font-family: var(--font--SemiBold);
+	}
 
-  &:hover {
-    outline: 1px solid var(--main-color);
-    background-color: #ffffff;
-    span {
-      color: var(--main-color);
-    }
-  }
+	&:hover {
+		outline: 1px solid var(--main-color);
+		background-color: #ffffff;
+		span {
+			color: var(--main-color);
+		}
+	}
 `;
