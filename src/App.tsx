e3492f7c19c9a54dd481376577895f9734c4533b
starcradle101 <dreamerdev12@gmail.com>
2023-12-08 21:34:52 +0900

    ♻️Refactor: App.tsx에서 인기 그룹 페이지 컴포넌트 제거(#57) SearchTrendingGroup 컴포넌트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { MyGlobalStyle } from './style/GlobalStyle';
 import { SearchMain } from './pages/search/SearchMainPage';
 import { SearchHistory } from 'pages/search/SearchHistoryPage';
 import { SearchByCategory } from './pages/search/SearchByCategoryPage';
-import { SearchTrendingGroup } from 'pages/search/SearchTrendingGroupPage';
 import { SearchTrendingFolder } from 'pages/search/SearchTrendingFolderPage';
 import { ProfilePage } from './pages/profile/ProfilePage';
 import { ProfileUpdatePage } from 'pages/profile/ProfileUpdatePage';
@@ -25,42 +24,38 @@ import { UpdateData } from 'components/crud/UpdateData';
 import { DeleteData } from 'components/crud/DeleteData';
 
 export function App(): JSX.Element {
-  return (
-    // <TestDb/>
-    <RecoilRoot>
-      <BrowserRouter>
-        <MyGlobalStyle />
-        <Routes>
-          <Route path="/" element={<Test />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<SignUP />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/postupload" element={<PostTS />} />
-          <Route path="/profilePage" element={<ProfilePage />} />
-          <Route path="/searchmain" element={<SearchMain />} />
-          <Route
-            path="/searchmain/trendingfolder"
-            element={<SearchTrendingFolder />}
-          />
-          <Route
-            path="/searchmain/trendinggroup"
-            element={<SearchTrendingGroup />}
-          />
-          <Route path="/searchhistory" element={<SearchHistory />} />
-          <Route path="/searchbycategory" element={<SearchByCategory />} />
-          <Route path="/profileUpdatePage" element={<ProfileUpdatePage />} />
-          <Route
-            path="/profiledetailPage/:currentStep"
-            element={<ProfileGroupDetailPage />}
-          />
-          <Route path="/com" element={<SignUpCompletedPage />} />
-          <Route path="/mainpostview" element={<MainPostViewPage />} />
-          <Route path="/createdata" element={<CreateData />} />
-          <Route path="/readdata" element={<ReadData />} />
-          <Route path="/updatedata" element={<UpdateData />} />
-          <Route path="/deletedata" element={<DeleteData />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
-  );
+	return (
+		// <TestDb/>
+		<RecoilRoot>
+			<BrowserRouter>
+				<MyGlobalStyle />
+				<Routes>
+					<Route path='/' element={<Test />} />
+					<Route path='/home' element={<Home />} />
+					<Route path='/signup' element={<SignUP />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/postupload' element={<PostTS />} />
+					<Route path='/profilePage' element={<ProfilePage />} />
+					<Route path='/searchmain' element={<SearchMain />} />
+					<Route
+						path='/searchmain/trendingfolder'
+						element={<SearchTrendingFolder />}
+					/>
+					<Route path='/searchhistory' element={<SearchHistory />} />
+					<Route path='/searchbycategory' element={<SearchByCategory />} />
+					<Route path='/profileUpdatePage' element={<ProfileUpdatePage />} />
+					<Route
+						path='/profiledetailPage/:currentStep'
+						element={<ProfileGroupDetailPage />}
+					/>
+					<Route path='/com' element={<SignUpCompletedPage />} />
+					<Route path='/mainpostview' element={<MainPostViewPage />} />
+					<Route path='/createdata' element={<CreateData />} />
+					<Route path='/readdata' element={<ReadData />} />
+					<Route path='/updatedata' element={<UpdateData />} />
+					<Route path='/deletedata' element={<DeleteData />} />
+				</Routes>
+			</BrowserRouter>
+		</RecoilRoot>
+	);
 }
