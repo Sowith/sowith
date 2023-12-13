650b47c11334108ea69aca7303d4a678774ceb4b
starcradle101 <dreamerdev12@gmail.com>
2023-12-13 21:51:55 +0900

    🛠️Feat: 회원가입 시 사용자 정보 추가 기능 구현(#22) 회원가입 시 Authentication 외에도 사용자의 정보를 firestore의 user collection에 추가하는 기능 구현

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,60 +1,98 @@
-import { useState } from "react";
-import { appAuth } from "../firebase/config";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from 'react';
+import { appAuth, appFireStore } from '../firebase/config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
+
+interface UserProfileDocument {
+	uid: string;
+	userId: string;
+	userName: string;
+	description: string;
+	createdFolders: string[];
+	bookmarkedFolders: string[];
+	followers: string[];
+	following: string[];
+	groups: string[];
+	posts: string[];
+	profileImageURL: string;
+	searchHistories: {
+		title: string;
+		relatedPost?: number;
+		historyCategory: 'tag' | 'user' | 'text';
+		followerCount?: number;
+	}[];
+}
 
 interface SignUpData {
-  email: string;
-  password: string;
-  userName: string;
-  accountID: string;
+	email: string;
+	password: string;
+	userName: string;
+	accountID: string;
 }
 
 interface SignUpHook {
-  error: string | null;
-  isPending: boolean;
-  signUpHook: (signUpData: SignUpData) => void;
+	error: string | null;
+	isPending: boolean;
+	signUpHook: (signUpData: SignUpData) => void;
 }
 
 export const useSignUpHook = (): SignUpHook => {
-  const [error, setError] = useState<string | null>(null);
-  const [isPending, setIsPending] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+	const [isPending, setIsPending] = useState<boolean>(false);
 
-  const signUpHook = (signUpData: SignUpData) => {
-    setError(null);
-    setIsPending(true);
-    const myEmail = signUpData.email;
-    const myPassword = signUpData.password;
-    const myName = signUpData.userName;
-    const myAccountID = signUpData.accountID;
+	const signUpHook = async (signUpData: SignUpData) => {
+		setError(null);
+		setIsPending(true);
+		const myEmail = signUpData.email;
+		const myPassword = signUpData.password;
+		const myName = signUpData.userName;
+		const myAccountID = signUpData.accountID;
 
-    createUserWithEmailAndPassword(appAuth, myEmail, myPassword)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        if (!user) {
-          throw new Error("회원가입에 실패했습니다.");
-        }
+		try {
+			const userCredential = await createUserWithEmailAndPassword(
+				appAuth,
+				myEmail,
+				myPassword
+			);
+			const user = userCredential.user;
 
-        const customUserData = {
-          displayName: myAccountID,
-          userRealName: myName,
-        };
+			if (!user) {
+				throw new Error('회원가입에 실패했습니다.');
+			}
 
-        updateProfile(appAuth.currentUser, customUserData)
-          .then(() => {
-            setError(null);
-            setIsPending(false);
-            console.log("customUserData", customUserData);
-          })
-          .catch((e) => {
-            setError(e.message);
-            setIsPending(false);
-          });
-      })
-      .catch((e) => {
-        setError(e.message);
-        setIsPending(false);
-      });
-  };
+			const customUserData = {
+				displayName: myAccountID,
+				userRealName: myName,
+			};
 
-  return { error, isPending, signUpHook };
+			const customUserDocument: UserProfileDocument = {
+				uid: user.uid,
+				userId: myAccountID,
+				userName: myName,
+				description: '',
+				createdFolders: [],
+				bookmarkedFolders: [],
+				followers: [],
+				following: [],
+				groups: [],
+				posts: [],
+				profileImageURL: '',
+				searchHistories: [],
+			};
+
+			const userProfileDocRef = doc(appFireStore, 'users', user.uid);
+			await setDoc(userProfileDocRef, customUserDocument);
+
+			await updateProfile(appAuth.currentUser, customUserData);
+
+			setError(null);
+			setIsPending(false);
+			console.log('회원가입 및 사용자 데이터 추가가 완료되었습니다.');
+		} catch (e: any) {
+			setError(e.message);
+			setIsPending(false);
+		}
+	};
+
+	return { error, isPending, signUpHook };
 };
