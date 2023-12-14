66d23f08f57d316280d1ba2cd8a29b872d0d3878
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-12-15 00:43:38 +0900

    :recycle: Refactor: 회원가입 시 유저이름에 관한 키워드 배열 생성 (#53)

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,60 +1,100 @@
-import { useState } from "react";
-import { appAuth } from "../firebase/config";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from 'react';
+import { appAuth } from '../firebase/config';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { useFirestoreCreate } from './useFirestoreCreate';
+import { useCreateKeywords } from './useCreateKeywords';
+
+interface UserProfileDocument {
+	// uid: string;
+	accountId: string;
+	accountName: string;
+  accountIdKeywords: string[];
+  accountNameKeywords: string[];
+	description: string;
+	createdFolders: string[];
+	// bookmarkedFolders: string[];
+	followers: string[];
+	following: string[];
+	posts: string[];
+	profileImageURL: string;
+	searchHistories: {
+		title: string;
+		relatedPost?: number;
+		historyCategory: 'tag' | 'user' | 'text';
+		followerCount?: number;
+	}[];
+}
 
 interface SignUpData {
-  email: string;
-  password: string;
-  userName: string;
-  accountID: string;
+	email: string;
+	password: string;
+	userName: string;
+	accountID: string;
 }
 
 interface SignUpHook {
-  error: string | null;
-  isPending: boolean;
-  signUpHook: (signUpData: SignUpData) => void;
+	error: string | null;
+	isPending: boolean;
+	signUpHook: (signUpData: SignUpData) => void;
 }
 
 export const useSignUpHook = (): SignUpHook => {
-  const [error, setError] = useState<string | null>(null);
-  const [isPending, setIsPending] = useState<boolean>(false);
 
-  const signUpHook = (signUpData: SignUpData) => {
-    setError(null);
-    setIsPending(true);
-    const myEmail = signUpData.email;
-    const myPassword = signUpData.password;
-    const myName = signUpData.userName;
-    const myAccountID = signUpData.accountID;
+  const { generateKeywordCombinations } = useCreateKeywords();
 
-    createUserWithEmailAndPassword(appAuth, myEmail, myPassword)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        if (!user) {
-          throw new Error("회원가입에 실패했습니다.");
-        }
+	const [error, setError] = useState<string | null>(null);
+	const [isPending, setIsPending] = useState<boolean>(false);
+	const { CreateDocumentWithCustomID } = useFirestoreCreate('users');
 
-        const customUserData = {
-          displayName: myAccountID,
-          userRealName: myName,
-        };
+	const signUpHook = async (signUpData: SignUpData) => {
+		setError(null);
+		setIsPending(true);
+		const myEmail = signUpData.email;
+		const myPassword = signUpData.password;
+		const myName = signUpData.userName;
+		const myAccountID = signUpData.accountID;
 
-        updateProfile(appAuth.currentUser, customUserData)
-          .then(() => {
-            setError(null);
-            setIsPending(false);
-            console.log("customUserData", customUserData);
-          })
-          .catch((e) => {
-            setError(e.message);
-            setIsPending(false);
-          });
-      })
-      .catch((e) => {
-        setError(e.message);
-        setIsPending(false);
-      });
-  };
+		try {
+			const userCredential = await createUserWithEmailAndPassword(
+				appAuth,
+				myEmail,
+				myPassword
+			);
+			const user = userCredential.user;
+			console.log(user);
 
-  return { error, isPending, signUpHook };
+			if (!user) {
+				throw new Error('회원가입에 실패했습니다.');
+			}
+
+			const customUserDocument: UserProfileDocument = {
+				// uid: user.uid,
+				accountId: myAccountID,
+				accountName: myName,
+        accountIdKeywords: generateKeywordCombinations(myAccountID),
+        accountNameKeywords: generateKeywordCombinations(myName),
+				description: '',
+				createdFolders: [],
+				// bookmarkedFolders: [],
+				followers: [],
+				following: [],
+				posts: [],
+				profileImageURL: '',
+				searchHistories: [],
+			};
+
+			CreateDocumentWithCustomID(user.uid, customUserDocument);
+
+			await updateProfile(appAuth.currentUser, { displayName: myAccountID });
+
+			setError(null);
+			setIsPending(false);
+			console.log('회원가입 및 사용자 데이터 추가가 완료되었습니다.');
+		} catch (e: any) {
+			setError(e.message);
+			setIsPending(false);
+		}
+	};
+
+	return { error, isPending, signUpHook };
 };
