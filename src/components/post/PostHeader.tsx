2f6138325f9a9eade6650f149d6614321585109f
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2023-12-19 20:33:11 +0900

    :hammer_and_wrench: Feat: 게시물 작성 완료 후 관련 컬랙션에 게시물 uid 저장 로직 추가 (#53)

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -5,9 +5,11 @@ import { useFirestoreCreate } from "hooks/useFirestoreCreate";
 import { useFirestoreUpdate } from "hooks/useFirestoreUpdate";
 import { useCreateKeywords } from "hooks/useCreateKeywords";
 import { useImageUpload } from "hooks/useImageUpload";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import postFormState from "recoil/postFormState";
-import { v4 as uuidv4 } from 'uuid';
+import { Timestamp } from 'firebase/firestore';
+import { arrayUnion } from "firebase/firestore";
+
 
 import { useAlertControl } from "hooks/useAlertControl";
 import { AlertBox } from "components/common/AlertBox";
@@ -15,6 +17,7 @@ import { Button } from "../common/Button";
 
 import { ReactComponent as IconArrow } from "../../assets/icon/icon-back-arrow.svg";
 import { ReactComponent as IconCamera } from "../../assets/icon/icon-camera.svg";
+import { useEffect } from "react";
 
 interface HeaderProps {
   content: string;
@@ -26,63 +29,94 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = (props) => {
 
-  const randomDoc = uuidv4()
+  const [postForm, setPostForm] = useRecoilState(postFormState);
 
-  const token = sessionStorage.getItem('token');
-  const uid = token !== null ? JSON.parse(token).uid : null;
-
-  const { openAlert, AlertComponent } = useAlertControl();
-
-  const postForm = useRecoilValue(postFormState);
-
-  const navigate = useNavigate();
-
+  const { CreateDocument } = useFirestoreCreate('posts');
   const { imagesUpload } = useImageUpload()
-  const { CreateDocumentManual } = useFirestoreCreate('posts');
   const { generateKeywordCombinations } = useCreateKeywords();
+  const { openAlert, AlertComponent } = useAlertControl();
+  const navigate = useNavigate();
+  const createdAt = Timestamp.fromDate(new Date());
 
-  // const updateTags = () => {
-  //   const { UpdateField } = useFirestoreUpdate('tags');
-  //   UpdateField(postForm.hashtag, 
-  //   {
-  //     tagNameKeywords: generateKeywordCombinations(),
-  //     taggedFolderIDs: null,
-  //     taggedPostIDs: randomDoc,
-  //   });
-  // }
+  const UpdateTags = (postId) => {
+    const { UpdateField } = useFirestoreUpdate('tags');
+    const { CreateDocumentWithCustomID } = useFirestoreCreate('tags');
 
-  // const updateField = () => {
-  //   const { UpdateField } = useFirestoreUpdate('folders');
-  //   UpdateField('', 
-  //   {
-  //     uidContainers: uid,
-  //   });
-  // }
+    postForm.hashtag.map(async (item) => {
+      const isExistTag = await UpdateField(
+        {
+          taggedPostIDs: arrayUnion(postId),
+        }, item, false);
+      !isExistTag && CreateDocumentWithCustomID(item, {
+        createdAt: createdAt,
+        tagNameKeywords: generateKeywordCombinations(item),
+        taggedFolderIDs: [],
+        taggedPostIDs: arrayUnion(postId),
+      })
+    })
+  }
+
+  const UpdateFolders = (postId) => {
+    const { UpdateField } = useFirestoreUpdate('folders');
+    postForm.folder.map((item) => {
+      UpdateField(
+        {
+          postUids: arrayUnion(postId),
+        }, item.id);
+    })
+  }
+
+  const resetPostForm = () => {
+    setPostForm({
+      picture: [],
+      phrase: '',
+      location: '',
+      folder: [],
+      hashtag: [],
+      usertag: []
+    })
+  }
+
+  const isEmpty = (item) => {
+    for (let key in item) {
+      if (Array.isArray(item[key]) && item[key].length > 0) {
+        return false;
+      } else if (typeof item[key] === 'string' && item[key].trim() !== "") {
+        return false;
+      }
+      return true;
+    }
+  }
+
+  useEffect(() => {
+    !isEmpty(postForm) && openAlert();
+  }, [])
 
   const handleUpload = async () => {
     const uploadPromises = postForm.picture.map((item) => {
       return imagesUpload("post", item.src, item.filter);
     });
 
-
     try {
-      // 모든 이미지 업로드를 기다림
       const downloadURLs = await Promise.all(uploadPromises);
 
-      CreateDocumentManual({
+      const postId = await CreateDocument({
         comments: [],
         content: postForm.phrase,
         images: downloadURLs,
         likedUsers: [],
         location: postForm.location,
         hashtags: postForm.hashtag,
-        tagUsers: postForm.usertag,
-        uuidv4
-      }, randomDoc);
+        tagUsers: postForm.usertag.map(item => item.id),
+      });
+
+      UpdateTags(postId)
+      UpdateFolders(postId)
+
+      resetPostForm()
 
     } catch (error) {
       console.error("이미지 업로드 중 오류 발생:", error);
-      // 오류 처리
     }
   };
 
@@ -106,12 +140,13 @@ export const Header: React.FC<HeaderProps> = (props) => {
     props.setStep(Prev => Prev === 1 ? Prev - 1 : Prev)
   }
 
+
   return (
     <>
       <AlertComponent>
-        {props.step === 2 && !postForm.location ?
-          <AlertBox alertMsg={"위치 지정은 필수입니다."} choice={["확인"]} /> :
-          <AlertBox alertMsg={"사진 선택은 필수입니다."} choice={["확인"]} />}
+        {props.step === 0 && postForm.picture.length === 0 && <AlertBox alertMsg={"사진 선택은 필수입니다."} choice={["확인"]} />}
+        {props.step === 0 && !isEmpty(postForm) && <AlertBox alertMsg={"이전에 작성하던 데이터가 있습니다."} choice={["불러오기", "새로작성"]} handleFunc={() => resetPostForm()} />}
+        {props.step === 1 && !postForm.location && <AlertBox alertMsg={"사진 선택은 필수입니다."} choice={["확인"]} />}
       </AlertComponent>
       <WrapStyle>
         <IconArrow width={30} onClick={handleGoBack} />
