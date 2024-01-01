6bfc70460f2ef17d8bd575cff3b4890856c4467a
starcradle101 <dreamerdev12@gmail.com>
2024-01-01 15:44:55 +0900

    ♻️Refactor: Firestore hook 일부 변경(#70) * useFirestoreRead 훅의 경우 type 구체화 * useFirestoreDelete 훅의 경우 컨벤션을 바탕으로 문법 수정

--- src/hooks/useFirestoreDelete.tsx
+++ src/hooks/useFirestoreDelete.tsx
@@ -3,7 +3,6 @@ import { doc, deleteDoc, getDoc, updateDoc } from 'firebase/firestore';
 import getUserInfo from 'utils/getUserInfo';
 
 export const useFirestoreDelete = (collectionName) => {
-
 	const uid = getUserInfo();
 
 	const DeleteDocument = async (documentId) => {
@@ -16,10 +15,10 @@ export const useFirestoreDelete = (collectionName) => {
 					// 문서 삭제
 					await deleteDoc(docRef);
 					console.log('데이터가 성공적으로 삭제되었습니다');
-					return true
+					return true;
 				} else {
 					console.error('해당 문서를 찾을 수 없거나 권한이 없습니다');
-					return false
+					return false;
 				}
 			} else {
 				console.error('UID가 없습니다');
@@ -35,28 +34,34 @@ export const useFirestoreDelete = (collectionName) => {
 		elementsToDelete: any = [],
 		attributesToDelete: any = []
 	) => {
-
 		try {
 			if (uid !== null) {
 				const docRef = doc(appFireStore, collectionName, documentId);
 				const docSnapshot = await getDoc(docRef);
 
-				if (docSnapshot.exists() && docSnapshot.data().userId === uid) {
+				if (docSnapshot.exists() && docSnapshot.data().uid === uid) {
 					const existingData = docSnapshot.data();
 					const arrayField = existingData[fieldName];
 
 					if (Array.isArray(arrayField)) {
-						const elementsToDeleteArray = Array.isArray(elementsToDelete) ? elementsToDelete : [elementsToDelete];
+						const elementsToDeleteArray = Array.isArray(elementsToDelete)
+							? elementsToDelete
+							: [elementsToDelete];
 
-						const newArrayField = arrayField.filter(item => {
+						const newArrayField = arrayField.filter((item) => {
 							if (typeof item === 'object') {
 								return !attributesToDelete.every((attribute, index) => {
 									if (attribute === 'createdAt') {
 										const createdAtTimestamp = {
 											seconds: item[attribute].seconds,
-											nanoseconds: item[attribute].nanoseconds
+											nanoseconds: item[attribute].nanoseconds,
 										};
-										return createdAtTimestamp.seconds === (elementsToDeleteArray[index] as any).seconds && createdAtTimestamp.nanoseconds === (elementsToDeleteArray[index] as any).nanoseconds;
+										return (
+											createdAtTimestamp.seconds ===
+												(elementsToDeleteArray[index] as any).seconds &&
+											createdAtTimestamp.nanoseconds ===
+												(elementsToDeleteArray[index] as any).nanoseconds
+										);
 									}
 									return item[attribute] === elementsToDeleteArray[index];
 								});
@@ -68,8 +73,9 @@ export const useFirestoreDelete = (collectionName) => {
 						updateData[fieldName] = newArrayField;
 
 						await updateDoc(docRef, updateData);
-						console.log(`필드 '${fieldName}'의 일부 원소가 성공적으로 삭제되었습니다`);
-
+						console.log(
+							`필드 '${fieldName}'의 일부 원소가 성공적으로 삭제되었습니다`
+						);
 					} else {
 						const updateData = {};
 						updateData[fieldName] = null;
@@ -86,10 +92,7 @@ export const useFirestoreDelete = (collectionName) => {
 		} catch (error) {
 			console.error('원소 삭제를 실패했습니다:', error);
 		}
-	}
-
+	};
 
 	return { DeleteDocument, DeleteField };
 };
-
-
--- src/hooks/useFirestoreRead.tsx
+++ src/hooks/useFirestoreRead.tsx
@@ -1,45 +1,62 @@
 import { appFireStore } from '../firebase/config';
-import { collection, query, orderBy, where, getDocs, doc, getDoc } from 'firebase/firestore';
+import {
+	collection,
+	query,
+	orderBy,
+	where,
+	getDocs,
+	doc,
+	getDoc,
+	OrderByDirection,
+} from 'firebase/firestore';
 
 export const useFirestoreRead = (collectionName) => {
-  const ReadAllDocument = async (fieldName: any = false, sortOrder: any = false) => {
+	const ReadAllDocument = async (
+		fieldName?: string,
+		sortOrder?: OrderByDirection
+	) => {
+		const collectionRef = collection(appFireStore, collectionName);
+		const querySnapshot =
+			fieldName === undefined || sortOrder === undefined
+				? await getDocs(collectionRef)
+				: await getDocs(query(collectionRef, orderBy(fieldName, sortOrder)));
 
-    const collectionRef = collection(appFireStore, collectionName);
-    const querySnapshot = fieldName ? await getDocs(collectionRef) : await getDocs(query(collectionRef, orderBy(fieldName, sortOrder)))
+		const result = querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			data: doc.data(),
+		}));
 
-    const result = querySnapshot.docs.map(doc => ({
-      id: doc.id,
-      data: doc.data()
-    }));
-    return result;
-  }
+		return result;
+	};
 
+	const ReadDocument = async (documentName) => {
+		const docRef = doc(appFireStore, collectionName, documentName);
+		const docSnapshot = await getDoc(docRef);
 
-  const ReadDocument = async (documentName) => {
-    const docRef = doc(appFireStore, collectionName, documentName);
-    const docSnapshot = await getDoc(docRef);
+		if (docSnapshot.exists()) {
+			const result = { id: docSnapshot.id, data: docSnapshot.data() };
+			return result;
+		} else {
+			console.error('해당 문서를 찾을 수 없습니다: ', documentName);
+			return false;
+		}
+	};
 
-    if (docSnapshot.exists()) {
-      const result = { id: docSnapshot.id, data: docSnapshot.data() };
-      return result;
-    } else {
-      console.error('해당 문서를 찾을 수 없습니다: ', documentName);
-      return false;
-    }
-  }
+	// where 사용 유형
+	// where('tag', '!=', []) tag 필드 안에 (배열)값이 들어있는 것들만 가져오기
 
-  // where 사용 유형
-  // where('tag', '!=', []) tag 필드 안에 (배열)값이 들어있는 것들만 가져오기
-
-  const ReadField = async (fieldName, operator, value) => {
-    const q = query(collection(appFireStore, collectionName), where(fieldName, operator, value))
-    const querySnapshot = await getDocs(q)
-    const result = querySnapshot.docs.map(doc => ({
-      id: doc.id,
-      data: doc.data()
-    }));
-    return result;
-  }
+	const ReadField = async (fieldName, operator, value) => {
+		const q = query(
+			collection(appFireStore, collectionName),
+			where(fieldName, operator, value)
+		);
+		const querySnapshot = await getDocs(q);
+		const result = querySnapshot.docs.map((doc) => ({
+			id: doc.id,
+			data: doc.data(),
+		}));
+		return result;
+	};
 
 	return { ReadAllDocument, ReadField, ReadDocument };
 };
