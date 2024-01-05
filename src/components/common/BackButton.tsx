9dc85a765cddb49385d39b44c6ab686029fb3cdf
starcradle101 <dreamerdev12@gmail.com>
2024-01-05 18:38:10 +0900

    ♻️Refactor: BackButton 컴포넌트 로직 수정(#75) * 뒤로 가기 버튼의 로직을 수정해, /search 로 시작하는 경로에서 뒤로 가기를 클릭할 경우 /searchmain 경로로 이동하도록 수정

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import arrowBack from '../../assets/icon/icon-arrow.svg';
 
 export const BackButton: React.FC = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  const handleBackButtonClick = (): void => {
-    navigate(-1);
-  };
+	const handleBackButtonClick = (): void => {
+		if (location.pathname.startsWith('/search/')) {
+			navigate('/searchmain');
+		} else {
+			navigate(-1);
+		}
+	};
 
-  return <StyledBackButton onClick={handleBackButtonClick} />;
+	return <StyledBackButton onClick={handleBackButtonClick} />;
 };
 
 const StyledBackButton = styled.button`
-  background-image: url(${arrowBack});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  width: 20px;
-  height: 20px;
-  cursor: pointer;
-  margin-right: 10px;
+	background-image: url(${arrowBack});
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: contain;
+	width: 20px;
+	height: 20px;
+	cursor: pointer;
+	margin-right: 10px;
 `;
